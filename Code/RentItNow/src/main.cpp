c71f8a1126737c043584d88e54b7f867ce09c07c
Raffaele Berzoini <raffaele.berzoini@txtgroup.com>
2024-10-27 19:42:11 +0100

    Boss logic to manage users in final application

--- Code/RentItNow/src/main.cpp
+++ Code/RentItNow/src/main.cpp
@@ -12,7 +12,33 @@ int main()
     API::Common::AddUser(user);
 
     user.address = "456 Main St";
-//API::Common::AddUser(user);
+    API::Common::AddUser(user);
+
+    Interfaces::User user2("Mary", "Jane", "789 Main St", "1234 5678 9999 3456", "AA0012");
+    API::Common::AddUser(user2);
+
+    auto users = API::Common::GetAllUsers();
+
+    for (const auto& user : users)
+    {
+        std::cout << user.name << " " << user.surname << " " << user.address << " " << user.drivingLicense << " "
+                  << user.creditCard << std::endl;
+    }
+
+    // Modify user
+    user.creditCard = "6543 2109 8765 4321";
+    API::Common::UpdateUser("AA0011", user);
+
+    // Remove user
+    API::Common::RemoveUser("AA0012");
+
+    users = API::Common::GetAllUsers();
+
+    for (const auto& user : users)
+    {
+        std::cout << user.name << " " << user.surname << " " << user.address << " " << user.drivingLicense << " "
+                  << user.creditCard << std::endl;
+    }
 
     return 0;
 }
