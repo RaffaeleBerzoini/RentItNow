630cbb796f4467575e2655e64b85c1a1007dbcea
Raffaele Berzoini <raffaele.berzoini@txtgroup.com>
2024-10-27 18:51:06 +0100

    Remove obsolete commented-out code in UserManagerTest.cpp
    The commented-out block containing the implementation of methods
    in the `UserManager` class, such as `AddUser`, `UpdateUser`,
    `RemoveUser`, `GetUser`, and the constructor, has been removed.
    These methods were responsible for user management operations
    and interactions with a `DatabaseManager`. Their removal suggests
    that these implementations are no longer needed in this file,
    possibly due to relocation or refactoring.

--- Code/UnitTests/src/UserManager/UserManagerTest.cpp
+++ Code/UnitTests/src/UserManager/UserManagerTest.cpp
@@ -3,62 +3,6 @@
 #include "UserManager.h"
 #include <filesystem>
 
-/*
-#include <iostream>
-#include "UserManager.h"
-
-bool UserManager::AddUser(const User& user)
-{
-    if (dbManager->AddUser(user))
-    {
-        std::cout << "User " << user.drivingLicense << " added" << std::endl;
-        return true;
-    }
-    else
-    {
-        std::cout << "User not added" << std::endl;
-        return false;
-    }
-}
-
-bool UserManager::UpdateUser(const std::string& driving_license, const User& user)
-{
-    if (dbManager->UpdateUser(driving_license, user))
-    {
-        std::cout << "User " << user.drivingLicense << " updated" << std::endl;
-        return true;
-    }
-    else
-    {
-        std::cout << "User not updated" << std::endl;
-        return false;
-    }
-}
-
-bool UserManager::RemoveUser(const std::string& drivingLicense)
-{
-    if (dbManager->RemoveUser(drivingLicense))
-    {
-        std::cout << "User " << drivingLicense << " removed" << std::endl;
-        return true;
-    }
-    else
-    {
-        std::cout << "User not removed" << std::endl;
-        return false;
-    }
-}
-
-std::optional<User> UserManager::GetUser(const std::string& drivingLicense)
-{
-    return dbManager->GetUser(drivingLicense);
-}
-
-UserManager::UserManager(std::shared_ptr<DatabaseManager> dbManagerPtr)
-    : dbManager(dbManagerPtr)
-{}
-
-*/
 
 TEST_CASE("UserManagerTest")
 {
